cf05cb2740ab7261fcfeded97c9a0fefbe997e76
kavaleriy <begemot.lery@mail.ru>
2014-06-24 01:36:25 -0700

    Data entering

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-    @area_list = Area.all.map { |area| [area.name, area.id] }
   end
 
   def map_os
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'phones/index'
+
   resources :housings
 
   resources :types
